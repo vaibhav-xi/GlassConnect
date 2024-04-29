9c14a65fa18d246921feee4aa0da1e47729c0204
Vaibhav <vaibhav28890@gmail.com>
2024-04-29 16:09:43 +0530

    modified onwrite

--- GlassConnect/GlassConnect.ino
+++ GlassConnect/GlassConnect.ino
@@ -11,13 +11,13 @@ bool isConnectedToWifi() {
 }
 
 class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      // You can add connection logic here
-    }
+  void onConnect(BLEServer* pServer) {
+    Serial.println("Device connected via Bluetooth.");
+  }
 
-    void onDisconnect(BLEServer* pServer) {
-      // You can add disconnection logic here
-    }
+  void onDisconnect(BLEServer* pServer) {
+    Serial.println("Device disconnected.");
+  }
 };
 
 void sendResultToApp(BLECharacteristic* pCharacteristic, const char* message) {
@@ -25,73 +25,81 @@ void sendResultToApp(BLECharacteristic* pCharacteristic, const char* message) {
     pCharacteristic->notify();
 }
 
+void onWrite(BLECharacteristic* pCharacteristic) {
+  Serial.println("Received Something");
+
+  std::string value = pCharacteristic->getValue();
+
+  if (value.length() > 0) {
+      Serial.println("Received value:");
+      Serial.println(value.c_str()); // Print the received value
+
+      // Extract WiFi credentials and attempt connection
+      // Format: ssid,password
+      String ssid, password;
+      int commaIndex = value.find(",");
+      if (commaIndex != -1) {
+          ssid = value.substr(0, commaIndex).c_str();
+          password = value.substr(commaIndex + 1).c_str();
+          
+          Serial.println("Attempting to connect to WiFi...");
+          if (WiFi.begin(ssid.c_str(), password.c_str()) == WL_CONNECTED) {
+              Serial.println("Connected to WiFi.");
+              sendResultToApp(pCharacteristic, "WiFi connection successful.");
+          } else {
+              Serial.println("Failed to connect to WiFi.");
+              sendResultToApp(pCharacteristic, "Unable to connect to WiFi.");
+          }
+      }
+  }
+}
+
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Initialize WiFi
-    WiFi.begin();
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
+  // Initialize WiFi
+  WiFi.begin();
+
+  unsigned long startTime = millis(); // Start time
+  const unsigned long timeout = 30000;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (millis() - startTime >= timeout) {
+      Serial.println("WiFi connection timeout.");
+      break; // Exit loop if timeout is reached
     }
 
-    if (!isConnectedToWifi()) {
-        // Initialize BLE
-        BLEDevice::init("GlassConnect"); // Set Bluetooth name here
-        BLEServer *pServer = BLEDevice::createServer();
-        pServer->setCallbacks(new MyServerCallbacks());
-        BLEService *pService = pServer->createService(SERVICE_UUID);
-        BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                               CHARACTERISTIC_UUID,
-                                               BLECharacteristic::PROPERTY_READ |
-                                               BLECharacteristic::PROPERTY_WRITE |
-                                               BLECharacteristic::PROPERTY_NOTIFY |
-                                               BLECharacteristic::PROPERTY_INDICATE
-                                           );
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-        pService->start();
+  if (!isConnectedToWifi()) {
+    // Initialize BLE
+    BLEDevice::init("GlassConnect"); // Set Bluetooth name here
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                            CHARACTERISTIC_UUID,
+                                            BLECharacteristic::PROPERTY_READ |
+                                            BLECharacteristic::PROPERTY_WRITE |
+                                            BLECharacteristic::PROPERTY_NOTIFY |
+                                            BLECharacteristic::PROPERTY_INDICATE
+                                        );
 
-        // Advertise service
-        BLEAdvertising *pAdvertising = pServer->getAdvertising();
-        pAdvertising->addServiceUUID(pService->getUUID());
-        pAdvertising->start();
+    pService->start();
 
-        Serial.println("Waiting for connections...");
-    } else {
-        Serial.println("Connected to WiFi.");
-    }
+    // Advertise service
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->addServiceUUID(pService->getUUID());
+    pAdvertising->start();
+
+    Serial.println("Waiting for connections...");
+  } else {
+    Serial.println("Connected to WiFi.");
+  }
 }
 
 void loop() {
     // Your loop code
-}
-
-void onWrite(BLECharacteristic* pCharacteristic) {
-    std::string value = pCharacteristic->getValue();
-
-    if (value.length() > 0) {
-        Serial.println("Received value:");
-        for (int i = 0; i < value.length(); i++) {
-            Serial.print(value[i]);
-        }
-        Serial.println();
-
-        // Extract WiFi credentials and attempt connection
-        // Format: ssid,password
-        String ssid, password;
-        int commaIndex = value.find(",");
-        if (commaIndex != -1) {
-            ssid = value.substr(0, commaIndex).c_str();
-            password = value.substr(commaIndex + 1).c_str();
-            
-            Serial.println("Attempting to connect to WiFi...");
-            if (WiFi.begin(ssid.c_str(), password.c_str()) == WL_CONNECTED) {
-                Serial.println("Connected to WiFi.");
-                sendResultToApp(pCharacteristic, "WiFi connection successful.");
-            } else {
-                Serial.println("Failed to connect to WiFi.");
-                sendResultToApp(pCharacteristic, "Unable to connect to WiFi.");
-            }
-        }
-    }
 }
